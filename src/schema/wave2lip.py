ff3feb35ee820fe05accbb3076070a3676e4c348
lakiet1609 <kiettuanminhla1609@gmail.com>
2024-09-21 17:09:06 +0700

    update project structure

---
+++ src/schema/wave2lip.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class InputWave2lip(BaseModel):
+    checkpoint_path: str,
+    face_path: str,
+    audio_path=str,
+
+class ResultWave2lip:
+    outfile: str
+
+class OutputWave2lip:
+    success: bool
+    message: str
+    data: Optional[ResultWave2lip]
+
+
